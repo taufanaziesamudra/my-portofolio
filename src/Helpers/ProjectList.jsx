b43f58e060221d68c7ee56ad2981cca20021a5d7
Taufan Ajie Samudro <taufanaziesamudra@gmail.com>
2022-11-22 22:33:33 +0700

    update portofolio

--- src/Helpers/ProjectList.jsx
+++ src/Helpers/ProjectList.jsx
@@ -1,57 +1,57 @@
-import Project1 from "../Assets/a.jpeg";
+import Project1 from "../Assets/a.jpg";
 import Project2 from "../Assets/b.jpg";
-import Project3 from "../Assets/c.jpg";
-import Project4 from "../Assets/d.jpg";
-import Project5 from "../Assets/e.jpeg";
-import Project6 from "../Assets/f.jpg";
-import Project7 from "../Assets/g.jpg";
-import Project8 from "../Assets/h.jpeg";
-import Project9 from "../Assets/i.jpg";
+// import Project3 from "../Assets/c.jpg";
+// import Project4 from "../Assets/d.jpg";
+// import Project5 from "../Assets/e.jpeg";
+// import Project6 from "../Assets/f.jpg";
+// import Project7 from "../Assets/g.jpg";
+// import Project8 from "../Assets/h.jpeg";
+// import Project9 from "../Assets/i.jpg";
 
 export const ProjectList = [
   {
-    name: "Project1",
+    name: "CRUD-Users",
     image: Project1,
-    skills: "ReactJs, HTML, CSS, JavaScript",
+    skills: "ReactJs, NodeJs, Bulma CSS, JavaScript, MySql",
   },
   {
-    name: "Project2",
+    name: "CRUD-With Images",
     image: Project2,
-    skills: "ReactJs, HTML, CSS, JavaScript",
+    skills: "ReactJs, NodeJs, Bulma CSS, JavaScript, MySql",
   },
-  {
-    name: "Project3",
-    image: Project3,
-    skills: "ReactJs, HTML, CSS, JavaScript",
-  },
-  {
-    name: "Project4",
-    image: Project4,
-    skills: "ReactJs, HTML, CSS, JavaScript",
-  },
-  {
-    name: "Project5",
-    image: Project5,
-    skills: "ReactJs, HTML, CSS, JavaScript",
-  },
-  {
-    name: "Project6",
-    image: Project6,
-    skills: "ReactJs, HTML, CSS, JavaScript",
-  },
-  {
-    name: "Project7",
-    image: Project7,
-    skills: "ReactJs, HTML, CSS, JavaScript",
-  },
-  {
-    name: "Project8",
-    image: Project8,
-    skills: "ReactJs, HTML, CSS, JavaScript",
-  },
-  {
-    name: "Project9",
-    image: Project9,
-    skills: "ReactJs, HTML, CSS, JavaScript",
-  },
+  // {
+  //   name: "Project3",
+  //   image: Project3,
+  //   skills: "ReactJs, HTML, CSS, JavaScript",
+  // },
+  // {
+  //   name: "Project4",
+  //   image: Project4,
+  //   skills: "ReactJs, HTML, CSS, JavaScript",
+  // },
+  // {
+  //   name: "Project5",
+  //   image: Project5,
+  //   skills: "ReactJs, HTML, CSS, JavaScript",
+  // },
+  // {
+  //   name: "Project6",
+  //   image: Project6,
+  //   skills: "ReactJs, HTML, CSS, JavaScript",
+  // },
+  // {
+  //   name: "Project7",
+  //   image: Project7,
+  //   skills: "ReactJs, HTML, CSS, JavaScript",
+  // },
+  // {
+  //   name: "Project8",
+  //   image: Project8,
+  //   skills: "ReactJs, HTML, CSS, JavaScript",
+  // },
+  // {
+  //   name: "Project9",
+  //   image: Project9,
+  //   skills: "ReactJs, HTML, CSS, JavaScript",
+  // },
 ];
